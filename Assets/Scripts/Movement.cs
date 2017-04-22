a627a8e5ac72893e04e78f8d60c4e12b4438a94b
Sushiy <julivizi@yahoo.de>
2017-04-22 02:23:30 +0200

    Implemented two-stick Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed =  10.0F;
+
+    public bool isControlledByLeftStick = true;
+
+    PlayerController controller;
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponentInParent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float axisX, axisY;
+        if (isControlledByLeftStick)
+        {
+            axisX = controller.leftStickX;
+            axisY = controller.leftStickY;
+        }
+        else
+        {
+            axisX = controller.rightStickX;
+            axisY = controller.rightStickY;
+        }
+
+        float xMove = axisX * speed * Time.deltaTime;
+        float zMove = -axisY * speed * Time.deltaTime;
+
+        transform.Translate(xMove, 0, zMove, Space.World);
+        transform.LookAt(transform.position + new Vector3(xMove, 0, zMove) * 10.0f);
+      
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerIndex = 0;
+
+    public GameObject realPlayerPrefab, ghostPlayerPrefab;
+
+    [HideInInspector]
+    public float leftStickX, leftStickY, rightStickX, rightStickY, triggerAxis;
+
+    private bool isXbox360Ctrl = true;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        GameObject.Instantiate(realPlayerPrefab, transform.position, transform.rotation, transform);
+        GameObject.Instantiate(ghostPlayerPrefab, transform.position, transform.rotation, transform);
+        string joystickName = Input.GetJoystickNames()[playerIndex];
+        
+        switch(joystickName)
+        {
+            case "Controller (XBOX 360 For Windows)":
+                isXbox360Ctrl = true;
+                break;
+            default:
+                isXbox360Ctrl = false;
+                break;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //X and Y axis are always the first 2
+        leftStickX = Input.GetAxis("XAxis_" + playerIndex);
+        leftStickY = Input.GetAxis("YAxis_" + playerIndex);
+
+        if(isXbox360Ctrl)
+        {
+            //If this controller is xbox360, the trigger axis is axis3 and rightstick is 4,5
+            triggerAxis = Input.GetAxis("Axis3_" + playerIndex);
+            rightStickX = Input.GetAxis("Axis4_" + playerIndex);
+            rightStickY = Input.GetAxis("Axis5_" + playerIndex);
+        }
+        else
+        {
+            //Otherwise the trigger axis is axis5 and rightstick is 3,4
+            rightStickX = Input.GetAxis("Axis3_" + playerIndex);
+            rightStickY = Input.GetAxis("Axis4_" + playerIndex);
+            triggerAxis = Input.GetAxis("Axis5_" + playerIndex);
+        }
+
+    }
+}
